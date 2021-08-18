22ef8c4ecb8c2a0fac25766e281c9184c4a0c73a
Skipcast <jonathanjl99@gmail.com>
2021-08-18 23:12:54 +0200

    Added project files

---
+++ JKMP.Plugin.Multiplayer/MultiplayerPlugin.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace JKMP.Plugin.Multiplayer
+{
+    // ReSharper disable once UnusedType.Global
+    public class MultiplayerPlugin : Core.Plugins.Plugin
+    {
+        public MultiplayerPlugin()
+        {
+        }
+    }
+}
