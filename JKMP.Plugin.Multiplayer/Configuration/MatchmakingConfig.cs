1e825b2bb6ba354bcf8128b181dd740918c053c5
Skipcast <jonathanjl99@gmail.com>
2022-01-13 19:27:07 +0100

    Changed default matchmaking config values

--- JKMP.Plugin.Multiplayer/Configuration/MatchmakingConfig.cs
+++ JKMP.Plugin.Multiplayer/Configuration/MatchmakingConfig.cs
@@ -5,10 +5,10 @@ namespace JKMP.Plugin.Multiplayer.Configuration
     public class MatchmakingConfig
     {
         [JsonRequired]
-        public string Endpoint { get; set; } = "localhost";
+        public string Endpoint { get; set; } = "jkmp-backend-matchmaking.fly.dev";
 
         [JsonRequired]
-        public ushort Port { get; set; } = 16000;
+        public ushort Port { get; set; } = 10069;
 
         public string Password { get; set; } = "";
     }
