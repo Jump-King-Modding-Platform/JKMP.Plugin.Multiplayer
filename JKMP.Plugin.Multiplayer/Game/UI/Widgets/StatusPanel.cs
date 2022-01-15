5162d2ed1f1192e1a1f6601c728c5e1172ac4a8c
Skipcast <jonathanjl99@gmail.com>
2022-01-15 19:45:41 +0100

    Added a status panel that shows total players on server and in group (currently only shows placeholder data)

---
+++ JKMP.Plugin.Multiplayer/Game/UI/Widgets/StatusPanel.cs
@@ -0,0 +1,61 @@
+using JKMP.Plugin.Multiplayer.Matchmaking;
+using JKMP.Plugin.Multiplayer.Networking;
+using JumpKing.PauseMenu;
+using Myra.Graphics2D.UI;
+
+namespace JKMP.Plugin.Multiplayer.Game.UI.Widgets
+{
+    public class StatusPanel : ResourceWidget<StatusPanel>
+    {
+        public string TotalPlayers
+        {
+            get => totalPlayers.Text;
+            set => totalPlayers.Text = value;
+        }
+
+        public string GroupPlayers
+        {
+            get => groupPlayers.Text;
+            set => groupPlayers.Text = value;
+        }
+
+        public bool Connected
+        {
+            get => connected;
+            set
+            {
+                if (value == connected)
+                    return;
+                
+                connected = value;
+                
+                connectedContainer.Visible = connected;
+                disconnectedContainer.Visible = !connected;
+            }
+        }
+
+        private readonly Label totalPlayers;
+        private readonly Label groupPlayers;
+        private readonly Widget connectedContainer;
+        private readonly Widget disconnectedContainer;
+        private bool connected;
+
+        public StatusPanel() : base("UI/Status/StatusPanel.xmmp")
+        {
+            totalPlayers = EnsureWidgetById<Label>("TotalPlayers");
+            groupPlayers = EnsureWidgetById<Label>("GroupPlayers");
+            connectedContainer = EnsureWidgetById<Widget>("ConnectedContainer");
+            disconnectedContainer = EnsureWidgetById<Widget>("DisconnectedContainer");
+
+            connectedContainer.Visible = false;
+            
+            AcceptsKeyboardFocus = false;
+        }
+
+        public void Update(float delta)
+        {
+            Visible = PauseManager.instance.IsPaused;
+            Connected = MatchmakingManager.Instance.IsConnected;
+        }
+    }
+}
